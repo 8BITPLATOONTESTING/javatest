794a915da0a81ee4d64a175f9b6990ccc9241402
8bptest <8bptesting@gmail.com>
2014-04-03 10:47:22 +0200

    helper system.out.println class
    with some cool command line 8BP

--- src/com/platoon/learning/HelloWorld.java
+++ src/com/platoon/learning/HelloWorld.java
@@ -6,8 +6,19 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		zz.doPrint = false;
+		zz.o("Hello World!");
 
+		zz.doPrint = true;
+		zz.o("This is 8 BIT PLATOON.... and then SPARTA!");
+		zz.o(" _");   	 
+		zz.o("|_||_  _");
+		zz.o("|_||_||_|");
+  	    zz.o("      |");
+		
 	}
+
+	
 
 }
---
+++ src/com/platoon/learning/zz.java
@@ -0,0 +1,18 @@
+package com.platoon.learning;
+
+/**
+ * @author Luigi
+ */
+
+public class zz {
+
+	public static boolean doPrint = true;
+	
+	public static void o(String str) {
+		
+		if (doPrint == true)
+			System.out.println(str);
+		
+	}
+	
+}
